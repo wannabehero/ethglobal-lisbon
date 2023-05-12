2f10b555609c615cc8fbeb0d79eda7736bdca4a1
Timur Abdukerimov <trabdukerimov@gmail.com>
2023-05-13 00:36:08 +0100

    [web] initial setup for web3

---
+++ webapp/src/web3/contracts.ts
@@ -0,0 +1,8 @@
+import { BigNumber, Contract, ethers } from 'ethers';
+
+type Provider = ethers.providers.Provider;
+type Signer = ethers.Signer;
+
+// calls to abis
+
+// TODO: Add get your contract addresses here
